9a4b4864bdf6cc7eae60049429cf662b7bab111c
Alex <alexandre.bordais@gmail.com>
2025-11-24 14:44:12 +0100

    version modified

--- version.tf
+++ version.tf
@@ -1,10 +1,8 @@
 terraform {
-  required_version = ">= 1.5"
-
   required_providers {
     aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.0"
+      source = "hashicorp/aws"
     }
   }
+  required_version = ">= 1.12"
 }
